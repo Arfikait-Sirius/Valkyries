6118feaf974eb8d012a2b5cfa839e1650ca98e74
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2021-10-23 17:19:27 +0900

    Retake Louise. - Make up.

--- dir_Girls/Louise/Louise.rs
+++ dir_Girls/Louise/Louise.rs
@@ -1,11 +1,32 @@
+//------------------------
+// :[ NAME ]:
+//     fn_upper_all
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
 pub fn fn_upper_all( base: String ) -> String {
     return base.to_uppercase() ;
 }
 
+//------------------------
+// :[ NAME ]:
+//     fn_lower_all
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
 pub fn fn_lower_all( base: String ) -> String {
     return base.to_lowercase() ;
 }
 
+//------------------------
+// :[ NAME ]:
+//     fn_upper_first
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
 pub fn fn_upper_first( base: String ) -> String {
     let base_upper = base.to_uppercase() ;
     let base_lower = base.to_lowercase() ;
